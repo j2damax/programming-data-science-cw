9e193fad21e633b24d311c2b7f9734d6056acd9c
Jayampathy Balasuriya <jam@vfullstack.com>
2025-03-15 14:49:04 +0530

    created Staff and Student sub classes

---
+++ src/Staff.py
@@ -0,0 +1,14 @@
+# Staff.py
+
+from person import Person
+
+
+class Staff(Person):
+    """
+    Sub class representing a Staff member
+    Staff sub class inhertics common attributes and methods from common class Person
+    """
+
+    def __init__(self, name, age, address, staff_id):
+        super.__init__(name, age, address)
+        self.staff_id = staff_id
