3f4cbb1a637f12eb9f9594cd7550df3f4a8c6066
Jayampathy Balasuriya <jam@vfullstack.com>
2025-03-15 14:30:46 +0530

    Created Person class

---
+++ src/Person.py
@@ -0,0 +1,16 @@
+# Person.py
+
+"""
+Base class representing a persin with common attributes and methods 
+"""
+
+"""
+Define common attributes 
+"""
+def __ini__(self, name, age, address):
+    self.name = name
+    self.age = age
+    self.address = address
+
+
+
